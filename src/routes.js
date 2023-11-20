c4db53c2a8810dc3e23738774db3290688420cde
arifadhitya <arifadhitya111@gmail.com>
2023-11-20 23:30:00 +0800

    notes inih

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByHandler } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByHandler,
+    },
+];
+
+module.exports = routes;
